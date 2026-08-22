3b60d965b912cc19dfdde164253f892c16fe5c74
Ayyub <ayyubxonmashrapov@gmail.com>
2026-08-22 16:12:27 +0500

    Superadmin datas

--- src/core/config/index.ts
+++ src/core/config/index.ts
@@ -4,5 +4,9 @@ config()
 
 export const env = {
     PORT : Number(process.env.PORT),
-    DB_URI : String(process.env.DB_URI)
+    DB_URI : String(process.env.DB_URI),
+    SUPERADMIN : {
+        PHONE : String(process.env.SUPERADMIN_PHONE),
+        PASSWORD : String(process.env.SUPERADMIN_PASSWORD)
+    }
 }
