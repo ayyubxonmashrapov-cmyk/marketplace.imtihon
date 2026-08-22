aee4ad2ff815f43d3a924176e13c82a03552a4fc
Ayyub <ayyubxonmashrapov@gmail.com>
2026-08-22 16:13:35 +0500

    superadmin creation

--- src/core/database/prisma.service.ts
+++ src/core/database/prisma.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from "@nestjs/commo
 import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "../config";
 import { PrismaClient } from "../../../generated/prisma/client";
+import { AdminRoles } from "../../common/enum/admin-roles";
+import { Crypt } from "../../infrastructure/lib/Crypt";
 
 @Injectable()
 export class PrismaService
@@ -23,6 +25,32 @@ export class PrismaService
     async onModuleInit(): Promise<void> {             //promice nimaga kerak ekanligini bilmiman
         await this.$connect()
         this.logger.log('database is connected')
+
+
+        const isSuperAdmin = await this.admin.findFirst({
+            where : { 
+                role : AdminRoles.SUPERADMIN
+            }
+        });
+
+        if (!isSuperAdmin){
+            const user = await this.user.create({
+                data : {
+                    phone : env.SUPERADMIN.PHONE,
+                    hashedPassword : await Crypt.hash(env.SUPERADMIN.PASSWORD),
+                    fullName : 'Full Name'
+                }
+            });
+
+            await this.admin.create({
+                data : {
+                    userId : user.id,
+                    role : AdminRoles.SUPERADMIN
+                }
+            })
+
+            console.log('Superadmin is created')
+        }
     }
 
     async onModuleDestroy(): Promise<void> {
