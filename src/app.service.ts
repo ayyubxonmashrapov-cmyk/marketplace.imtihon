2a0f3f15b41bdb12886d3b6055961d5005b9e5f8
Ayyub <ayyubxonmashrapov@gmail.com>
2026-08-22 16:23:22 +0500

    globalpypes, helmet, prifix

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { env } from "./core/config";
+import helmet from "helmet"
 
 @Injectable()
 export class App {
   static async main() {
     const app = await NestFactory.create(AppModule);
     const PORT = env.PORT
+    const prefix = '/api/v1'
 
-    app.setGlobalPrefix('/api/v1')
+
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist : true,
+      forbidNonWhitelisted : true,
+      transform : true
+    }));
+
+    app.use(helmet)    
+
+    app.setGlobalPrefix(prefix)
 
     await app.listen(PORT, () => console.log('server is running on port', PORT));
   }
