15442553ee4ba8fdc58fabfacbb9f496d5b5bd21
Ayyub <ayyubxonmashrapov@gmail.com>
2026-08-22 18:41:16 +0500

    imports[AdminModule]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './core/database/prisma.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    AdminModule
+  ],
 })
 export class AppModule {}
